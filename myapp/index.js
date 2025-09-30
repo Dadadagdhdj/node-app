a58f7760d16962be3cbab297d39c459f13d43c5f
ddau <ddau@123>
2025-09-30 10:38:32 +0530

    update

--- myapp/index.js
+++ myapp/index.js
@@ -1,11 +1,13 @@
-index.js
-var express = require('express'); 
-  
-var app = express();//Respond with "hello world" for requests that hit our root "/" 
-app.get('/', function (req, res) { 
- res.send('changed not '); 
-});//listen to port 3000 by default 
-app.listen(3000, '0.0.0.0') => {
+
+var express = require('express');
+ 
+var app = express();//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+ res.send('changed not ');
+});//listen to port 3000 by default
+app.listen(3000, '0.0.0.0', function ()  {
   console.log('Server running on port 3000');
-}); 
+});
 module.exports = app;
+
+
